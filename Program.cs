2a173f71e81edd263ba59ed0cf09307ca6a45ae3
unknown <shaunak12wankhade@ggmail.com>
2021-10-17 17:40:15 +0530

    [Shaunak] Added : Edit contact in address book

--- Program.cs
+++ Program.cs
@@ -13,12 +13,34 @@ namespace Address_Book_System
             //Console.WriteLine("Welcome to address book system");
             //Console.ReadLine();
 
-            while (true)
-            {
-                Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
-                    " \n 3 for editing the existing contact \n 4 for deleting contact");
+            //while (true)
+            //{
+            //    Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
+            //        " \n 3 for editing the existing contact \n 4 for deleting contact");
 
-                int choice = int.Parse(Console.ReadLine());
+
+            //}
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    AddTheDetails obj = new AddTheDetails();
+                    obj.EnterInput();
+                    break;
+
+
+                case 2:
+                    AddTheDetails obj1 = new AddTheDetails();
+                    obj1.ViewTheDetails();
+                    break;
+
+
+                case 3:
+                    EditTheDetails obj2 = new EditTheDetails();
+                    obj2.EditUsingName();
+                    break;
+
+
             }
         }
     }
