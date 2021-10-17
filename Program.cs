6719c1135bdc7e4ca313f326f9bb2cf30d7f4004
unknown <shaunak12wankhade@ggmail.com>
2021-10-17 16:36:02 +0530

    [Shaunak] Added : Contacts details in address book

--- Program.cs
+++ Program.cs
@@ -10,8 +10,16 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to address book system");
-            Console.ReadLine();
+            //Console.WriteLine("Welcome to address book system");
+            //Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
+                    " \n 3 for editing the existing contact \n 4 for deleting contact");
+
+                int choice = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
---
+++ UC1Create a contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    class UC1Create_a_contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
